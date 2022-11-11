997068a8281995eb349d75532cfbe270fd6cb621
Mateus Braga <mateuscbraga@gmail.com>
2022-11-11 13:18:40 -0300

    chore: adds middlewares to their own folder

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,9 @@ import { IController } from './@types/controllers'
 import http, { Server as NativeServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
-import logger from './config/logger'
+import logger from '@config/logger'
 import DB from '@db/index'
+import { LogMiddleware } from '@middlewares/log-request'
 
 export default class App {
   public app: Application
@@ -36,13 +37,7 @@ export default class App {
     this.app.use(cors())
     this.initializeEvents()
 
-    this.app.use((req, _res, next) => {
-      logger.info('body: %o', req.body)
-      logger.info('params: %o', req.params)
-      logger.info('headers: %o', req.headers)
-
-      next()
-    })
+    this.app.use(new LogMiddleware().init)
   }
 
   private initializeEvents() {
